a6998a4a8fd60f48e4fbe1ea64a9a8ed583e0042
Absalom Cherinet <absalomcherinet@gmail.com>
2018-09-06 15:53:07 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -85,5 +85,12 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  return_arr = []
+  holiday_hash.each do |season, holiday|
+
+    holiday.each do |key, value|
+          return_arr<<holiday if value.include?("BBQ")
+    end
+  end
 
 end
