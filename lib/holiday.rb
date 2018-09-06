2a7a94c1036164674c0106f33d0582d8c7ec894f
Absalom Cherinet <absalomcherinet@gmail.com>
2018-09-06 15:19:24 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |key, value|
-      puts "  #{key.capitalize}: #{value.join(", ")}"
+      puts "  #{key.capitalize.gsub!('_',' ')}: #{value.join(", ")}"
     end
   end
 
